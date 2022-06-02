2b5334c43b17fa214db119cebb44c7d9392840a7
onely6721 <aesamokish@gmail.com>
2022-06-02 12:44:21 +0300

    Update users.controller.ts

---
+++ server/src/users/users.controller.ts
@@ -0,0 +1,8 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from '../auth/auth.service';
+
+@Controller('users')
+export class UsersController {
+
+
+}
